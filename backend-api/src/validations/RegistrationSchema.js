d6fc2d3799cdabd7d24632f9bd17416c62d66375
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-11 23:33:39 +0000

    feat: add yup schema for registration request body

---
+++ backend-api/src/validations/RegistrationSchema.js
@@ -0,0 +1,30 @@
+import * as yup from 'yup';
+
+const RegistrationSchema = yup.object({
+  body: yup.object({
+    username:
+      yup.string()
+        .required('Username is required.')
+        .min(3, 'Handle is too short - should be 3 chars minimum.')
+        .max(21, 'Handle is too long - should be 21 chars maximum.'),
+    email:
+      yup.string()
+        .required('Email is required.')
+        .email('Invalid email address.'),
+    password:
+      yup.string()
+        .required('Password is required.')
+        .min(8, 'Password is too weak - should be 8 chars minimum.')
+        .max(40, 'Password is too long - should be 21 chars maximum.')
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>])(?!.*\s).{8,}$/,
+          "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character."
+        ),
+    repeatedPassword:
+      yup.string()
+        .oneOf([yup.ref('password')], 'Passwords must match.')
+        .required('Password, must be confirm.')
+  })
+});
+
+export default RegistrationSchema;
