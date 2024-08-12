3ece363956ed2697550c51abb08c007d9d311a7a
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-12 23:51:48 +0000

    feat: add auth logout and confirmation routes

--- backend-api/src/auth/index.js
+++ backend-api/src/auth/index.js
@@ -1,16 +1,26 @@
 import { Router } from 'express';
 import UserLogin from './UserLogin.js';
 import UserSignup from './UserSignup.js';
-import RegistrationSchema from '../validations/RegistrationSchema.js'
-import requestDataValidation from '../middlewares/requestDataValidation.js'
+import RegistrationSchema from '../validations/RegistrationSchema.js';
+import LoginSchema from '../validations/LoginSchema.js';
+import requestDataValidation from '../middlewares/requestDataValidation.js';
+import UserLogout from './UserLogout.js';
+import EmailConfirmation from './EmailConfirmation.js';
 
 const router = Router();
 
-router.post('/api/register',
+router.post('/api/auth/register',
   requestDataValidation(RegistrationSchema),
   UserSignup.register);
 
-router.post('/api/login',
+router.post('/api/auth/login',
+  requestDataValidation(LoginSchema),
   UserLogin.login);
+
+router.get('/api/auth/logout',
+  UserLogout.logout);
+
+router.get('/api/auth/confirmation',
+  EmailConfirmation.confirmation);
 
 export default router;
