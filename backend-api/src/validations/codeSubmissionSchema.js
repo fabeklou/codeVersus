0344f177b27870fdf8dbd65e9a43d9ef2dc84703
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-12 23:29:25 +0000

    feat: add yup schema for code submission

---
+++ backend-api/src/validations/codeSubmissionSchema.js
@@ -0,0 +1,16 @@
+import * as yup from 'yup';
+
+const CodeSubmissionSchema = yup.object({
+  body: yup.object({
+    submission:
+      yup.string()
+        .required('A code submission is required.'),
+    language:
+      yup.string()
+        .required('Programming language name is required.'),
+    stdin:
+      yup.string('Input must be a string.')
+  })
+});
+
+export default CodeSubmissionSchema;
