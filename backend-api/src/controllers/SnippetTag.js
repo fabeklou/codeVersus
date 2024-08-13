ac72be099216a2e93a6f28d6b50d772d76e4d456
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-13 23:55:31 +0000

    feat: add SnippetTag controller

---
+++ backend-api/src/controllers/SnippetTag.js
@@ -0,0 +1,46 @@
+import TagModel from '../models/Tag.js';
+
+class SnippetTag {
+  static async createTag(tagName) {
+    try {
+      const tag = new TagModel({ name: tagName });
+      await tag.save();
+      return tag._id;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async getTagByName(tagName) {
+    try {
+      const tag = await TagModel.findOne({ name: tagName });
+      return tag;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async getTags(page, limit) {
+    try {
+      const tags = await TagModel.find()
+        .sort({ name: 1 })
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .lean();
+      return tags;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async getTagById(tagId) {
+    try {
+      const tag = await TagModel.findById({ _id: tagId });
+      return tag;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
+
+export default SnippetTag;
