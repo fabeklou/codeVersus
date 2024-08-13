746f9be2b8fba3d534bcb6043bb4c009808109c9
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-13 23:51:44 +0000

    feat: add post snippet route

--- backend-api/src/routes/index.js
+++ backend-api/src/routes/index.js
@@ -3,12 +3,23 @@ import SubmitCode from '../controllers/SubmitCode.js';
 import requestDataValidation from '../middlewares/requestDataValidation.js';
 import CodeSubmissionSchema from '../validations/codeSubmissionSchema.js';
 import checkAuthStatus from '../middlewares/checkAuthStatus.js';
-import FetchSubmissionResult from '../controllers/FetchSubmissionResult.js';
+import snippetSchema from '../validations/snippetSchema.js';
+import CodeSnippet from '../controllers/CodeSnippet.js';
+
 const router = Router();
+
+/** Code submission routes */
 
 router.post('/api/submissions',
   requestDataValidation(CodeSubmissionSchema),
   checkAuthStatus,
   SubmitCode.submissions);
+
+/** Snippet routes */
+
+router.post('/api/snippets/save',
+  requestDataValidation(snippetSchema),
+  checkAuthStatus,
+  CodeSnippet.saveCodeSnippet);
 
 export default router;
