be6957b36d02ac58dfeb1e3ae2147cf3ee4a208a
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-15 00:00:54 +0000

    feat: add callback to session.destroy

--- backend-api/src/auth/UserLogout.js
+++ backend-api/src/auth/UserLogout.js
@@ -1,7 +1,9 @@
 class UserLogout {
   static async logout(req, res) {
     if (req.session.loggedIn) {
-      req.session.destroy();
+      req.session.destroy((error) => {
+        if (error) res.status(400).json({ error: 'Unable to logout.' })
+      });
       return res.status(200).json({ message: 'User logged out successfully.' });
     }
 
