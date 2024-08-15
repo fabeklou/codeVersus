aff9f70663644f95fb516920a8787b738f1e8f68
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-15 11:39:03 +0000

    feat: add snippet routes

--- backend-api/src/routes/index.js
+++ backend-api/src/routes/index.js
@@ -4,6 +4,9 @@ import requestDataValidation from '../middlewares/requestDataValidation.js';
 import CodeSubmissionSchema from '../validations/codeSubmissionSchema.js';
 import snippetSchema from '../validations/snippetSchema.js';
 import CodeSnippet from '../controllers/CodeSnippet.js';
+import UpdateProfileSchema from '../validations/updateProfileSchema.js';
+import UserProfile from '../controllers/UserProfile.js';
+import UpdateSnippetSchema from '../validations/UpdateSnippetSchema.js';
 
 const router = Router();
 
@@ -13,10 +16,47 @@ router.post('/api/submissions',
   requestDataValidation(CodeSubmissionSchema),
   SubmitCode.submissions);
 
+/** User profile routes */
+
+router.get('/api/users/profile/:username',
+  UserProfile.getProfile);
+
+router.put('/api/users/profile',
+  requestDataValidation(UpdateProfileSchema),
+  UserProfile.updateProfile);
+
 /** Snippet routes */
 
-router.post('/api/snippets/save',
+router.post('/api/snippets',
   requestDataValidation(snippetSchema),
   CodeSnippet.saveCodeSnippet);
+
+router.get('/api/snippets',
+  CodeSnippet.getCodeSnippets);
+
+router.get('/api/snippets/:snippetId',
+  CodeSnippet.getCodeSnippetById);
+
+router.put('/api/snippets/:snippetId',
+  requestDataValidation(UpdateSnippetSchema),
+  CodeSnippet.updateCodeSnippetById);
+
+router.delete('/api/snippets/:snippetId',
+  CodeSnippet.deleteCodeSnippetById);
+
+router.get('/api/snippets/link/:snippetId',
+  CodeSnippet.getCodeSnippetLink);
+
+router.get('/api/snippets/link/:privateLink',
+  CodeSnippet.getCodeSnippetFromLink);
+
+router.patch('/api/snippets/link/:snippetId',
+  CodeSnippet.removeCodeSnippetLink);
+
+router.get('/api/snippets/public',
+  CodeSnippet.getPublicCodeSnippets);
+
+router.patch('/api/snippets/like/:snippetId',
+  CodeSnippet.likeUnlikeCodeSnippet);
 
 export default router;
