1c4527f7c404875a97697aaa4ca6585072b95423
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-17 01:40:56 +0000

    refactor: handle invalid/unsupported language

--- backend-api/src/controllers/SubmitCode.js
+++ backend-api/src/controllers/SubmitCode.js
@@ -5,10 +5,15 @@ class SubmitCode {
   static async submissions(req, res) {
     const { submission, language, stdin } = req.body;
     const judgeURL = process.env.JUDGE0_URL;
+    const languageId = findLanguageId(language);
+
+    if (!languageId) {
+      return res.status(400).json({ error: 'Invalid or unsupported language.' });
+    }
 
     axios.post(`${judgeURL}/submissions?base64_encoded=true&wait=true`, {
       source_code: Buffer.from(submission).toString('base64'),
-      language_id: findLanguageId(language),
+      language_id: languageId,
       stdin: stdin ? Buffer.from(stdin).toString('base64') : stdin
     }, {
       headers: {
