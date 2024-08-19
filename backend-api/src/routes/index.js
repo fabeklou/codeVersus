982c3cf229168b4d09e349cdcbd74dc07441b8b0
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-19 15:08:35 +0000

    doc: add jsdoc for /users and /users/friends routes

--- backend-api/src/routes/index.js
+++ backend-api/src/routes/index.js
@@ -13,23 +13,380 @@ const router = Router();
 
 /** Code submission routes */
 
+/**
+* @swagger
+* /api/submissions:
+*   post:
+*     summary: User code submission
+*     tags:
+*       - Code Submission
+*     requestBody:
+*       description: Code submission data
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               submission:
+*                 type: string
+*                 description: Code submited by the user
+*                 example: "print('Hello from Code Versus: ', input(), input(), input())"
+*               language:
+*                 type: string
+*                 description: Pragramming language of user's code submission
+*                 example: Python
+*               stdin:
+*                 type: string
+*                 description: Standard input for the code submission
+*                 example: "1\n3\n4"
+*
+*             required:
+*               - submission
+*               - language
+*     responses:
+*       200:
+*         description: Successful code execution.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 stdout:
+*                   type: string
+*                   example: SGVsbG8gZnJvbSBKdWRnZTA6ICAxIDMgNAo=
+*                 stderr:
+*                   type: string/null
+*                   example: null
+*                 token:
+*                   type: string
+*                   example: 1e7b1b7b-7b1e-4b7b-8b1e-1e7b1b7b1e7b
+*                 compile_output:
+*                   type: string/null
+*                   example: null
+*                 status:
+*                   type: object
+*                   properties:
+*                     description:
+*                       type: string
+*                       example: Accepted
+*                     id:
+*                       type: number
+*                       example: 3
+*                 message:
+*                   type: string/null
+*                   example: success.
+*       400:
+*         description: Invalid or unsupported language.
+*       401:
+*         description: Unauthorized.
+*       500:
+*         description: Internal server error
+*/
 router.post('/api/submissions',
   requestDataValidation(CodeSubmissionSchema),
   SubmitCode.submissions);
 
 /** User profile routes */
 
+/**
+* @swagger
+* /api/users/profile:
+*   get:
+*     summary: User get his profile
+*     tags:
+*       - Users
+*     responses:
+*       200:
+*         description: Logged in user profile.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 _id:
+*                   type: string
+*                 username:
+*                   type: string
+*                 email:
+*                   type: string
+*                 createdAt:
+*                   type: string
+*                 updatedAt:
+*                   type: string
+*                 userProfile:
+*                   type: object
+*                   properties:
+*                     bio:
+*                       type: string
+*                       example: code versus is so much fun.
+*                     interests:
+*                       type: array
+*                       items:
+*                         type: string
+*                     profilePicture:
+*                       type: string
+*                     githubLink:
+*                       type: string
+*                     xLink:
+*                       type: string
+*                     linkedinLink:
+*                       type: string
+*                     friends:
+*                       type: array
+*                       items:
+*                         type: string
+*       401:
+*         description: Unauthorized.
+*       404:
+*         description: User not found
+*       500:
+*         description: Internal server error
+*/
 router.get('/api/users/profile',
   UserProfile.getProfile);
 
+/**
+* @swagger
+* /api/users/profile:
+*   put:
+*     summary: User update his profile
+*     tags:
+*       - Users
+*     requestBody:
+*       description: User update profile data
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               bio:
+*                 type: string
+*                 description: User biography
+*                 example: code versus is so much fun.
+*               interests:
+*                 type: array
+*                 items:
+*                   type: string
+*                 description: User interests
+*                 example: ["coding", "reading"]
+*               githubLink:
+*                 type: string
+*                 description: User GitHub profile link
+*                 example: https://github.com/johndoe
+*               xLink:
+*                 type: string
+*                 description: User Twitter (X) profile link
+*                 example: https://twitter.com/johndoe
+*               linkedinLink:
+*                 type: string
+*                 description: User LinkedIn profile link
+*                 example: https://linkedin.com/in/johndoe
+*             required:
+*               - interests
+*     responses:
+*       200:
+*         description: User updated profile data.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 message:
+*                   type: string
+*                   example: Profile updated successfully.
+*                 userProfile:
+*                   type: object
+*                   properties:
+*                     bio:
+*                       type: string
+*                       example: My bio has now been updated.
+*                     interests:
+*                       type: array
+*                       items:
+*                         type: string
+*                     profilePicture:
+*                       type: string
+*                     githubLink:
+*                       type: string
+*                     xLink:
+*                       type: string
+*                     linkedinLink:
+*                       type: string
+*       400:
+*         description: Invalid payload.
+*       401:
+*         description: Unauthorized.
+*       404:
+*         description: User not found
+*       500:
+*         description: Internal server error
+*/
 router.put('/api/users/profile',
   requestDataValidation(UpdateProfileSchema),
   upload.single('profilePicture'),
   UserProfile.updateProfile);
 
+/**
+ * @swagger
+ * /api/users/friends:
+ *   get:
+ *     summary: User retrieve his friends
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           description: Page number
+ *           example: 1
+ *           default: 1
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           description: Number of friends per page
+ *           example: 10
+ *           default: 10
+ *       - in: query
+ *         name: interests
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: Filter friends by interests
+ *           example: ["coding", "reading"]
+ *       - in: query
+ *         name: query
+ *         schema:
+ *           type: string
+ *           description: Search query to find friends
+ *           example: john
+ *     responses:
+ *       200:
+ *         description: A list of the user's friends.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 total:
+ *                   type: integer
+ *                   example: 8
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         example: 1234
+ *                       name:
+ *                         type: string
+ *                         example: John Doe
+ *                       interests:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                         example: ["coding", "reading"]
+ *       400:
+ *         description: Bad request.
+ *       401:
+ *         description: Unauthorized.
+ *       404:
+ *         description: User not found.
+ *       500:
+ *         description: Internal server error.
+ */
 router.get('/api/users/friends',
   UserProfile.getFriends);
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: User retrieve other users
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           description: Page number
+ *           example: 1
+ *           default: 1
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           description: Number of users per page
+ *           example: 10
+ *           default: 10
+ *       - in: query
+ *         name: interests
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: Filter users by interests
+ *           example: ["coding", "reading"]
+ *       - in: query
+ *         name: query
+ *         schema:
+ *           type: string
+ *           description: Search query to find users
+ *           example: john
+ *     responses:
+ *       200:
+ *         description: A list of users found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 total:
+ *                   type: integer
+ *                   example: 8
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         example: 1234
+ *                       name:
+ *                         type: string
+ *                         example: John Doe
+ *                       interests:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                         example: ["coding", "reading"]
+ *       400:
+ *         description: Bad request.
+ *       401:
+ *         description: Unauthorized.
+ *       404:
+ *         description: User not found.
+ *       500:
+ *         description: Internal server error.
+ */
 router.get('/api/users',
   UserProfile.getUsers);
 
