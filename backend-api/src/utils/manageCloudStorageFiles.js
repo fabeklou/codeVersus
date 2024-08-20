add2586f2b3c5f1eddfae51bad28a78179345ab3
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-20 23:49:47 +0000

    fix: file upload to GCS and publicUrl gen

--- backend-api/src/utils/manageCloudStorageFiles.js
+++ backend-api/src/utils/manageCloudStorageFiles.js
@@ -1,35 +1,41 @@
-import path from 'path';
 import { Storage } from '@google-cloud/storage';
 
 const projectId = process.env.GCP_PROJECT_ID;
 const bucketName = process.env.GCP_BUCKET_NAME;
-const keyfile = path.join(__dirname, '../../config/gcloud-storage-key.json');
+const keyfile = process.env.GCP_KEYFILE_PATH;
 
 const storage = new Storage({
   projectId,
-  keyFilename: path.join(__dirname, keyfile),
+  keyFilename: keyfile,
 });
 
 const sendToGCS = async (file, username) => {
-  const bucket = storage.bucket(bucketName);
-  const { originalname, buffer, mimetype } = file;
-  const blob = bucket.file(`profile-${username.replace(/ /g, '_')}`);
-  const blobStream = blob.createWriteStream({
-    metadata: {
-      contentType: mimetype,
-    },
-  });
+  return new Promise((resolve, reject) => {
+    const bucket = storage.bucket(bucketName);
+    const { originalname, buffer, mimetype } = file;
+    const blob = bucket.file(`profilePicture-${username.replace(/ /g, '_')}--${originalname}`);
+    const blobStream = blob.createWriteStream({
+      metadata: {
+        contentType: mimetype,
+      },
+    });
 
-  blobStream.on('error', (err) => {
-    throw new Error(err);
-  });
+    blobStream.on('error', (err) => {
+      reject(new Error(err));
+    });
 
-  blobStream.on('finish', async () => {
-    await blob.makePublic();
-    return `https://storage.googleapis.com/${bucketName}/${blob.name}`;
-  });
+    blobStream.on('finish', async () => {
+      try {
+        await blob.makePublic();
+        const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+        resolve(publicUrl);
+      } catch (err) {
+        reject(new Error(err));
+      }
+    });
 
-  blobStream.end(buffer);
+    blobStream.end(buffer);
+  });
 }
 
 const deleteFromGCS = async (url) => {
@@ -37,11 +43,11 @@ const deleteFromGCS = async (url) => {
   const bucket = storage.bucket(bucketName);
   const file = bucket.file(filename);
 
-  file.delete((err) => {
-    if (err) {
-      throw new Error(err);
-    }
-  });
+  try {
+    await file.delete();
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
 export { sendToGCS, deleteFromGCS };
